84a51e1764a17b8748f42b4f12bfa473b64b0e48
msi <meibrohimov@gmail.com>
2023-09-09 20:46:35 +0500

    add last change  👽

--- apps/admin.py
+++ apps/admin.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
 from import_export.admin import ImportExportModelAdmin
 from apps.models import Material
-from apps.app import ReportAdmin
 
 
 @admin.register(Material)
